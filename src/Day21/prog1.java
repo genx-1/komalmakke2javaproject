6f39ce53db30248da69b996ed608042c9983f439
genx-1 <komalmakke@gmail.com>
2025-05-30 16:33:44 +0530

    third commit

---
+++ src/Day21/prog1.java
@@ -0,0 +1,18 @@
+package Day21;
+
+public class prog1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//This is sample file
+		//This is sample file
+		//This is sample file
+		//This is sample file
+		//This is sample file
+		//This is sample file
+		//This is sample file
+		//This is sample file
+		//This is sample file
+	}
+
+}
